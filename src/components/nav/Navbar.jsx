ffa9aaa302de636edb8d54ccd83c87a322e72a6a
izzylogo <omitiranisrael@gmail.com>
2024-12-11 09:39:51 +0100

    changing

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import "@/components/nav/Navbar.scss"
 
 const Navbar = () => {
+
+  const [signup, setSignup] = useState(false)
+
+  const close = () => {
+    setSignup(false)
+  }
+  const open = () => {
+    setSignup(true)
+  }
+
   return (
     <div className='navbar'>
         <div className="navbar-contain">
@@ -11,9 +23,32 @@ const Navbar = () => {
                 <li>Shop</li>
                 <li>About</li>
                 <li>Blog</li>
+                <li>Add to cart</li>
             </ul>
-            <button>Add to cart</button>
+            <button onClick={open}>Sign Up</button>
         </div>
+
+        {/* sign up modal */}
+        {
+          signup ? 
+          <div className="sign-up-modal">
+            <div className="signup-content">
+              <form>
+                <label htmlFor="name">Full Name</label>
+                <input type="text" placeholder='Full Name' />
+                <label htmlFor="email">Email</label>
+                <input type="text" placeholder='Email'/>
+                <label htmlFor="password">Password</label>
+                <input type="text" placeholder='Password'/>
+              </form>
+              <div className="btns">
+                <button onClick={close}>Sign Up</button>
+                <button onClick={close}>Close</button>
+              </div>
+            </div>
+          </div> :
+          null
+        }
     </div>
   )
 }
